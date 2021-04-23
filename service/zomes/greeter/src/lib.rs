724b283bd4e76f81715021cb547ab84a1847dc51
Don Smith <hi@dons.dev>
2021-04-23 13:32:24 +1200

    wip: start add_thing function

--- service/zomes/greeter/src/lib.rs
+++ service/zomes/greeter/src/lib.rs
@@ -1,6 +1,30 @@
 use hdk::prelude::*;
 
+//
+// hello
+//
 #[hdk_extern]
 pub fn hello(_: ()) -> ExternResult<String> {
     Ok(String::from("Hello Holo Dev"))
+}
+
+//
+// add_thing
+//
+entry_defs![ThingEntry::entry_def()];
+
+#[hdk_entry(id = "thing")]
+pub struct ThingEntry {
+    name: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct NewThing {
+    name: String,
+}
+
+#[hdk_extern]
+pub fn add_thing(new_thing: NewThing) -> ExternResult<HeaderHash> {
+    let new_entry: ThingEntry = ThingEntry { name: new_thing.name };
+    create_entry(new_entry)
 }
